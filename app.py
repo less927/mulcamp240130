9d1a234a319036f158c4db4a5ec97b1a7474616b
user.email <soniccrush@naver.com>
2024-01-30 11:34:02 +0900

    yy

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 #-*- coding:utf-8 -*-
 
 import streamlit as st
+import seaborn as sns
+
+@st.cache_data
+def load_data():
+    df = sns.load_dataset("iris")
+    return df
 
 def main():
     st.title("hello world on streamlit.io")
+    iris = load_data()
+    st.table(iris)
     
     
 if __name__ == "__main__":
